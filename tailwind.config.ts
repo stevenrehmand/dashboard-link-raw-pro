d197f9e1b401b0be378864888cc37a5f2139d0e2
stevenrehmand <stevenrehmand@gmail.com>
2026-07-09 10:52:29 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ini akan men-scan semua file di src
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -9,14 +10,9 @@ const config: Config = {
       colors: {
         premium: {
           black: "#050505",
-          dark: "#0a0a0a",
-          gold: "#d4af37",
-          accent: "#3b82f6"
+          blue: "#3b82f6",
         }
       },
-      backgroundImage: {
-        'mesh': "radial-gradient(at 0% 0%, rgba(59, 130, 246, 0.15) 0, transparent 50%), radial-gradient(at 100% 100%, rgba(99, 102, 241, 0.15) 0, transparent 50%)",
-      }
     },
   },
   plugins: [],
