eb2fdb51c6f38e1ce9e2c5623f985bf3d1fb9d6b
stevenrehmand <stevenrehmand@gmail.com>
2026-07-09 11:22:37 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}", // Ini akan men-scan semua file di src
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        premium: {
-          black: "#050505",
-          blue: "#3b82f6",
-        }
-      },
+      colors: { premium: { black: "#020202", blue: "#3b82f6" } },
     },
   },
   plugins: [],
